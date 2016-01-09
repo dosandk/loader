61bbab753b03bc9bd9d5fc06edf2e794beff7591
Vladimir Shevchuk <dosandkv@gmail.com>
2016-01-09 18:55:47 +0700

    Add loader-queue. This loader generate loaders and streams for each one;

---
+++ loader-queue/loader-queue.js
@@ -0,0 +1,71 @@
+var App = {};
+
+App.loader = {
+    queue: [],
+    defaultQueueState: { counter: 0, isShown: false },
+    createLoader: function(level) {
+        console.error('createLoader');
+
+        var loaderContainer = document.getElementById('loader-container');
+        var loader = document.createElement('div');
+        loader.className = 'loader loader-' + level + ' hide';
+
+        loaderContainer.appendChild(loader);
+    },
+    initQueue: function (level) {
+        var self = this;
+
+        self.queue[level] = {};
+        self.queue[level].counter = 0;
+        self.queue[level].isShown = false;
+        self.createLoader(level);
+
+        console.log('queue', self.queue);
+    },
+    show: function (level) {
+        console.error('increaseCounter');
+        var self = this;
+        var currentLevel = typeof level !== 'undefined' ? level : 0;
+
+        if (typeof self.queue[currentLevel] === 'undefined') {
+            self.initQueue(currentLevel);
+        }
+
+        var currentQueue = self.queue[currentLevel];
+
+        console.log('currentLevel', currentLevel);
+        console.log('currentQueue', currentQueue);
+
+        currentQueue.counter += 1;
+
+        if (!currentQueue.isShown) {
+            $('.loader-' + currentLevel).removeClass('hide');
+            currentQueue.isShown = true;
+        }
+        console.log('queue', self.queue);
+    },
+    hide: function (level) {
+        console.error('decreaseCounter');
+        var self = this;
+        var currentLevel = typeof level !== 'undefined' ? level : 0;
+
+        if (typeof self.queue[currentLevel] === 'undefined') {
+            console.error('Current queue level: ' + currentLevel + ' does not exist');
+        }
+        else {
+            var currentQueue = self.queue[currentLevel];
+
+            console.log('currentLevel', currentLevel);
+            console.log('currentQueue', currentQueue);
+
+            currentQueue.counter ? currentQueue.counter -= 1 : currentQueue.counter = 0;
+            console.log('queue', self.queue);
+
+            if (!currentQueue.counter && currentQueue.isShown) {
+                $('.loader-' + currentLevel).addClass('hide');
+                currentQueue.isShown = false;
+                alert('success, level: ' + currentLevel);
+            }
+        }
+    }
+};
